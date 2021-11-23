687fd03aa5f34119c0df0bc8ab95bf8ed25c3781
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-23 15:47:22 +0100

    ex and bonus completed

---
+++ src/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="banner">
+    <Title msg="La mia prima app con Vue CLI!" />
+  </div>
+</template>
+
+<script>
+import Title from './Title.vue';
+export default {
+  name: 'Banner',
+  components: {
+    Title
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ #banner {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   width: 100%;
+   height: 500px;
+   background-image: url('../assets/coastal-wash-web.jpg');
+   background-repeat: no-repeat;
+   background-size: cover;
+   background-position: center;
+ }
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  font-size: 30px;
+  color: white;
+}
+</style>
